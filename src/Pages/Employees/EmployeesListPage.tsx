e25b6daa4e0a4aae7edd91b57f34a778d395eb6a
Bort190 <heiko.nevoigt@googlemail.com>
2026-04-09 19:53:33 +0200

    example implementation of tanstack table

--- src/Pages/Employees/EmployeesListPage.tsx
+++ src/Pages/Employees/EmployeesListPage.tsx
@@ -1,43 +1,137 @@
-import {useEffect, useState} from "react";
-import {getAllEmployees} from "@/axios/Endpoints/getAllEmployees.tsx";
-import type {Employee} from "@/axios/Types/EmployeeType.ts";
-import {Card} from "@/components/ui/card.tsx";
+import {useState} from "react";
+import {createColumnHelper, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import {Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
+
+//TData
+type User = {
+    firstName: string
+    lastName: string
+    age: number
+    visits: number
+    progress: number
+    status: string
+}
+
+const dummyData: User[] =
+    [
+        {
+            "firstName": "Tanner",
+            "lastName": "Linsley",
+            "age": 33,
+            "visits": 100,
+            "progress": 50,
+            "status": "Married"
+        },
+        {
+            "firstName": "Kevin",
+            "lastName": "Vandy",
+            "age": 27,
+            "visits": 200,
+            "progress": 100,
+            "status": "Single"
+        }
+    ]
+
+
+const columnHelper = createColumnHelper<User>()
+
+const columns = [
+    {
+        accessorKey: 'firstName',
+    },
+    columnHelper.accessor((row) => row.lastName, {
+        id: 'lastName',
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Last Name</span>,
+    }),
+    columnHelper.accessor('age', {
+        header: () => 'Age',
+        cell: (info) => info.renderValue(),
+    }),
+    columnHelper.accessor('visits', {
+        header: () => <span>Visits</span>,
+    }),
+    columnHelper.accessor('status', {
+        header: 'Status',
+    }),
+    columnHelper.accessor('progress', {
+        header: 'Profile Progress',
+    }),
+]
 
 const EmployeesListPage = () => {
-    const [employees, setEmployees] = useState<Employee[] | null>(null)
-    useEffect(() => {
-        getAllEmployees().then((res) => {
-                if (res.success) {
-                    setEmployees(res.data)
-                } else {
-                    console.log(res.error)
-                }
-            }
-        )
-    }, []);
+    const [data] = useState<User[]>(dummyData)
+    const [hasFooter] = useState<boolean>(false)
+
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    // const [employees, setEmployees] = useState<Employee[] | null>(null)
+    // useEffect(() => {
+    //     getAllEmployees().then((res) => {
+    //             if (res.success) {
+    //                 setEmployees(res.data)
+    //             } else {
+    //                 console.log(res.error)
+    //             }
+    //         }
+    //     )
+    // }, []);
 
     return (
-        <Card className={"w-full h-full bg-red-100"}>
-            <table>
-                <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Vorname</td>
-                    <td>Nachname</td>
-                </tr>
-                </thead>
-                <tbody>
-                {employees?.map((employee)=>(
-                    <tr key={employee.id}>
-                        <td>{employee.id}</td>
-                        <td>{employee.first_name}</td>
-                        <td>{employee.last_name}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </Card>
-    );
+        <div className="p-2 bg-gray-300">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext(),
+                                        )}
+                                </TableHead>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows.map((row) => (
+                        <TableRow key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <TableCell key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+                {hasFooter && <TableFooter>
+                    {table.getFooterGroups().map((footerGroup) => (
+                        <TableRow key={footerGroup.id}>
+                            {footerGroup.headers.map((header) => (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.footer,
+                                            header.getContext(),
+                                        )}
+                                </TableHead>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableFooter>}
+            </Table>
+        </div>
+    )
+
 };
 
 export default EmployeesListPage;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,71 @@
+import {type ColumnDef, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+
+export type TableDataType<T> = {
+    data: T;
+    columns: ColumnDef<TData>[]
+}
+
+
+
+const Table = ({data, columns}) => {
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+
+    return (
+        <div className="p-2">
+            <table>
+                <thead>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext(),
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody>
+                {table.getRowModel().rows.map((row) => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                            <td key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                {table.getFooterGroups().map((footerGroup) => (
+                    <tr key={footerGroup.id}>
+                        {footerGroup.headers.map((header) => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.footer,
+                                        header.getContext(),
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </tfoot>
+            </table>
+            <div className="h-4" />
+        </div>
+    )
+};
+
+export default Table;
